bd7e457d335e3bae8fbe28216556c126d7172b71
Kyle Kopicki <kkopick1@ford.com>
2017-04-26 16:05:53 -0400

    initial commit of /api/market-offer route. Responding with 10 objects with properties returned from the Java service, "pdo-web/api/marketOfferHeader/overview/1000004315"

---
+++ api/marketOffer.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+const faker = require('faker');
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  let jsonResponse = [];
+  for (let i = 0; i < 10; i++) {
+    jsonResponse.push({
+      id: faker.random.number(null, 10),
+      name: faker.random.arrayElement(['CAP 2015 Great Britain', 'TTA 2018 USA']),
+      mmView: { 
+        id: faker.random.number(null, 10),
+        name: faker.random.arrayElement(['CAP 2015 Great Britain MM', 'TTA 2018 USA MM']),
+        programCode: faker.random.arrayElement(['C214', 'C481', 'M582']),
+        ptvl: 'CAP 2015',
+        modelYear: '2015',
+        versioned: faker.random.boolean()
+      },
+      marketFeatureCode: null,
+      marketFeatureName: null,
+      mktGrpView: {
+        marketGroupName: 'UNITED KINGDOM', 
+        marketGroupCodes: [faker.random.arrayElement(['GREAT BRITAIN (WAEGB)', 'GREAT BRITAIN (WAEGN)'])]
+      },
+      versioned: faker.random.boolean()
+    });
+  }
+  res.json(jsonResponse);
+});
+
+module.exports = router;
