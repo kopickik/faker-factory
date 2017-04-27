273328a7e932a8c08f02b01b22fe85e89e99ec22
Kyle Kopicki <kkopick1@ford.com>
2017-04-27 11:35:18 -0400

    getting the id format correct

--- api/marketOffer.js
+++ api/marketOffer.js
@@ -9,10 +9,10 @@ router.get('/', function(req, res, next) {
   let jsonResponse = [];
   for (let i = 0; i < 10; i++) {
     jsonResponse.push({
-      id: faker.random.number(null, 10),
+      id: faker.random.number({min: 999999999, max: 10000000000}),
       name: faker.random.arrayElement(['CAP 2015 Great Britain', 'TTA 2018 USA']),
       mmView: { 
-        id: faker.random.number(null, 10),
+        id: faker.random.number({min: 999999999, max: 10000000000}),
         name: faker.random.arrayElement(['CAP 2015 Great Britain MM', 'TTA 2018 USA MM']),
         programCode: faker.random.arrayElement(['C214', 'C481', 'M582']),
         ptvl: 'CAP 2015',
